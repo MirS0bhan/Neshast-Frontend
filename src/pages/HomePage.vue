d79b43cc305f911f8089c979eae9129571f58caa
misano <mr.sobhan.n@outlook.com>
2025-04-09 00:22:03 +0330

    create main pages template and devide auth page

---
+++ src/pages/HomePage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Navbar from '@/components/layout/NavbarLayout.vue'
+import Footer from '@/components/layout/FooterLayout.vue'
+</script>
+
+<template>
+  <Navbar />
+  <div class="min-h-screen bg-gray-50">
+    <!-- <EventSection :events="events" /> -->
+
+    <Newsletter/>
+
+  </div>
+  <Footer />
+</template>
