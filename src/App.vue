c19d214f095ec29ba39ee855d4517390150d5d82
misano <mr.sobhan.n@outlook.com>
2025-04-09 00:23:48 +0330

    apply router and page logic

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,7 @@
 <script setup lang="ts">
-// import { ref } from 'vue';
-import Navbar from '@/components/Navbar.vue'
-import Footer from '@/components/Footer.vue'
-import Newsletter from '@/components/Newsletter.vue'
-import EventSection from '@/components/EventSection.vue';
-
-
-
-const events = [
-  {
-    id: 1,
-    title: 'جشنواره موسیقی تابستانی',
-    image: '/placeholder.svg?height=200&width=400',
-    price: '۴۹.۹۹ دلار',
-    date: '۱۵ جولای ۲۰۲۵',
-    time: '۴:۰۰ بعدازظهر',
-    location: 'پارک مرکزی',
-  },
-  {
-    id: 2,
-    title: 'کنفرانس فناوری ۲۰۲۵',
-    image: '/placeholder.svg?height=200&width=400',
-    price: '۱۴۹.۹۹ دلار',
-    date: '۱۰ آگوست ۲۰۲۵',
-    time: '۹:۰۰ صبح',
-    location: 'مرکز همایش‌ها',
-  },
-  // سایر رویدادها
-];
+// import HomePage from './pages/HomePage.vue'
 </script>
 
 <template>
-  <Navbar/>
-  <div class="min-h-screen bg-gray-50">
-    <EventSection :events="events" />
-
-    <Newsletter/>
-    <Footer/>
-  </div>
+    <RouterView />
 </template>
-
-
