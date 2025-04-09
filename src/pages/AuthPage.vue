9c1f9730b48a58393887b95abc02f33d0220e069
misano <mr.sobhan.n@outlook.com>
2025-04-10 01:22:56 +0330

    integrate whole auth in one file and update HomePage

---
+++ src/pages/AuthPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="w-full max-w-lg bg-white rounded-2xl shadow-lg p-6">
+      <TabsRoot v-model="activeTab">
+        <TabsList class="grid grid-cols-2 mb-6">
+          <TabsTrigger value="login">Login</TabsTrigger>
+          <TabsTrigger value="signup">Sign Up</TabsTrigger>
+        </TabsList>
+
+        <TabsContent value="login">
+          <LoginView />
+        </TabsContent>
+
+        <TabsContent value="signup">
+          <SignupView />
+        </TabsContent>
+      </TabsRoot>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+
+// reka-ui Tabs
+import {
+  TabsRoot,
+  TabsList,
+  TabsTrigger,
+  TabsContent,
+} from 'reka-ui'
+
+import LoginView from '#/layout/LoginLayout.vue'
+import SignupView from '#/layout/SignupLayout.vue'
+
+const activeTab = ref('login')
+</script>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,17 +1,34 @@
 <script setup lang="ts">
 import Hero from '@/components/layout/HeroLayout.vue';
 import BasePublicPage from './BasePublicPage.vue';
-import EventSection from '@/components/EventSection.vue';
+import UpcomingsLayout from '@/components/layout/UpcomingLayout.vue';
 import NewsletterLayout from '@/components/layout/NewsletterLayout.vue';
 
-const events = [{}]
+import EventRepo from '@/repos/EventsRepo'
+import { ref } from 'vue';
 
+interface Event {
+  id: number;
+  name: string;
+  date: string;
+}
+// Using .then() to handle the promise and assign it to a constant
+const events = ref({}); // Declare a variable outside
+
+EventRepo.getAllUpcomings()
+  .then(data => {
+    events.value = data; // Assign the resolved data to the variable
+    console.log(events); // Now events contains the resolved data
+  })
+  .catch(error => {
+    console.error('Error fetching events:', error);
+  });
 </script>
 
 <template>
   <BasePublicPage>
     <Hero/>
-    <EventSection :events="events" />
+    <UpcomingsLayout :events="events" />
     <NewsletterLayout/>
   </BasePublicPage>
 </template>
