db0bdd1d9650b55856a34a1420a837dd8f645e39
misano <mr.sobhan.n@outlook.com>
2025-04-09 12:24:07 +0330

    create base of public pages and improve home page

---
+++ src/pages/BasePublicPage.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import Navbar from '@/components/layout/NavbarLayout.vue'
+import Footer from '@/components/layout/FooterLayout.vue'
+</script>
+
+<template>
+  <Navbar />
+    <slot></slot>
+  <Footer />
+</template>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,15 +1,17 @@
 <script setup lang="ts">
-import Navbar from '@/components/layout/NavbarLayout.vue'
-import Footer from '@/components/layout/FooterLayout.vue'
+import Hero from '@/components/layout/HeroLayout.vue';
+import BasePublicPage from './BasePublicPage.vue';
+import EventSection from '@/components/EventSection.vue';
+import NewsletterLayout from '@/components/layout/NewsletterLayout.vue';
+
+const events = [{}]
+
 </script>
 
 <template>
-  <Navbar />
-  <div class="min-h-screen bg-gray-50">
-    <!-- <EventSection :events="events" /> -->
-
-    <Newsletter/>
-
-  </div>
-  <Footer />
+  <BasePublicPage>
+    <Hero/>
+    <EventSection :events="events" />
+    <NewsletterLayout/>
+  </BasePublicPage>
 </template>
