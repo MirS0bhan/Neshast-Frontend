5e3693e3b9167d5e78773743e81e4b60e59f684f
misano <mr.sobhan.n@outlook.com>
2025-04-10 19:09:40 +0330

    create dashboard base

---
+++ src/components/DashboardTab.vue
@@ -0,0 +1,25 @@
+<template>
+  <TabsTrigger value="a" class="flex items-center gap-4 p-4 bg-white rounded-2xl min-w-max ">
+    <component :is="icon" class="w-6 h-6 text-primary-500" />
+    <h2 class="text-xl font-semibold text-gray-800">
+      {{ title }}
+    </h2>
+  </TabsTrigger>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  value: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: Object,
+    required: true
+  }
+})
+</script>
---
+++ src/pages/DashboardPage.vue
@@ -0,0 +1,37 @@
+<template>
+  <NavbarLayout />
+  <div class="min-h-screen bg-[rgb(var(--color-primary))]">
+    <TabsRoot v-model="activeTab" class="flex gap-5 p-7" dir="rtl">
+      <TabsList class="flex-1 gap-1 rounded-2xl bg-white pt-4 pl-7">
+        <DashboardTab value="a" title="بلیط‌های من" :icon="TestIcon" />
+        <DashboardTab value="a" title="Featured" :icon="TestIcon" />
+        <DashboardTab value="a" title="Featurefdgsdfghsdd" :icon="TestIcon" />
+        <DashboardTab value="a" title="Featured" :icon="TestIcon" />
+        <DashboardTab value="a" title="Featured" :icon="TestIcon" />
+        <DashboardTab value="a" title="Featured" :icon="TestIcon" />
+        <DashboardTab value="a" title="Featured" :icon="TestIcon" />
+        <DashboardTab value="a" title="Featured" :icon="TestIcon" />
+      </TabsList>
+      <div class="min-h-screen flex-2/3 rounded-2xl bg-white p-10">
+        <TabsContent value="a">
+          <p>test</p>
+        </TabsContent>
+        <TabsContent value="b">
+          <p>test 213</p>
+        </TabsContent>
+      </div>
+    </TabsRoot>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { TabsRoot, TabsList, TabsContent } from 'reka-ui'
+
+import '@/assets/theme.css'
+import NavbarLayout from '@/components/layout/NavbarLayout.vue'
+import { ref } from 'vue'
+import DashboardTab from '@/components/DashboardTab.vue'
+import TestIcon from '@/components/icons/TestIcon.vue'
+
+const activeTab = ref('b')
+</script>
